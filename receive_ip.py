9bc8023a2a75dbfbe744050955776b98dcc5545b
bigzhu <vermiliondun@gmail.com>
2014-12-03 13:50:59 +0800

    server 返回 ip

--- receive_ip.py
+++ receive_ip.py
@@ -5,6 +5,9 @@ import tornado.web
 import tornado.autoreload
 
 
+the_ip = ''
+
+
 def getURLMap(the_globals):
     '''
         根据定义的tornado.web.RequestHandler,自动生成url map
@@ -14,7 +17,6 @@ def getURLMap(the_globals):
         try:
             if issubclass(the_globals[i], tornado.web.RequestHandler):
                 url_map.append((r'/' + i, the_globals[i]))
-                #url_map.append((r"/%s/([0-9]+)" % i, the_globals[i]))
                 url_map.append((r"/%s/(.*)" % i, the_globals[i]))
         except TypeError:
             continue
@@ -26,9 +28,19 @@ class setIP(tornado.web.RequestHandler):
     '''登录页面'''
 
     def get(self, ip):
-        print ip
+        global the_ip
+        the_ip = ip
         saveHosts(ip)
         self.write("ok")
+
+
+class getIP(tornado.web.RequestHandler):
+
+    '''登录页面'''
+
+    def get(self):
+        global the_ip
+        self.write(the_ip)
 
 
 def saveHosts(ip):
