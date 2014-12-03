57bb45be0f2735064ab09bfc52cd361f969713d9
bigzhu <vermiliondun@gmail.com>
2014-12-03 11:23:11 +0800

    ip 相同就不要发了

--- send_ip.py
+++ send_ip.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 import json
 import urllib2
+the_ip = ''
+
 
 def sendIP():
+    global the_ip
     ip = json.load(urllib2.urlopen('http://httpbin.org/ip'))['origin']
-    urllib2.urlopen('http://0.0.0.0:8009/setIP/%s' % ip)
+    if the_ip != ip:
+        urllib2.urlopen('http://42.96.204.146:8009/setIP/%s' % ip)
+        the_ip = ip
 
 if __name__ == '__main__':
     sendIP()
