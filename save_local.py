7058b2502ec83ad333355eb8eb6e26b403d0a1b9
bigzhu <vermiliondun@gmail.com>
2014-12-03 14:04:13 +0800

    增加可以写入本地的 save_local

--- save_local.py
+++ save_local.py
@@ -7,13 +7,17 @@ def saveHosts(ip):
     path_file = '/etc/hosts'
     with open(path_file) as f:
         lines = f.readlines()
-        print lines
         curr = lines[:-1]
         f.close()
     f = open(path_file, 'w')
     curr.append(u'%s mygit\n' % ip)
     f.writelines(curr)
+    print 'now hosts is:'
+    for i in curr:
+        print i
     f.close()
 
 if __name__ == '__main__':
-    urllib2.urlopen('http://42.96.204.146:9001/getIP')
+    ip = urllib2.urlopen('http://42.96.204.146:9001/getIP').read()
+    if ip != '':
+        saveHosts(ip)
