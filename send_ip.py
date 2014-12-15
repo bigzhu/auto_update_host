5e90b23e7237e652e268c06b02bad0f3d7873608
bigzhu <vermiliondun@gmail.com>
2014-12-15 17:20:49 +0800

    加入 timeout 避免卡住

--- send_ip.py
+++ send_ip.py
@@ -11,9 +11,9 @@ the_ip = ''
 
 def sendIP():
     global the_ip
-    ip = json.load(urllib2.urlopen('http://httpbin.org/ip'))['origin']
+    ip = json.load(urllib2.urlopen('http://httpbin.org/ip', timeout=5))['origin']
     if the_ip != ip:
-        urllib2.urlopen('http://42.96.204.146:9001/setIP/%s' % ip)
+        urllib2.urlopen('http://42.96.204.146:9001/setIP/%s' % ip, timeout=5)
         the_ip = ip
         print 'new ip = %s' % ip
     else:
@@ -22,9 +22,9 @@ def sendIP():
 if __name__ == '__main__':
     while True:
         print str(datetime.now())
-        #try:
-        sendIP()
-        #except Exception:
-        #    info = sys.exc_info()
-        #    print str(info[1])
+        try:
+            sendIP()
+        except Exception:
+            info = sys.exc_info()
+            print str(info[1])
         time.sleep(5)
