ba620bf0099addb7792337841aa2af525ac6ad80
bigzhu <vermiliondun@gmail.com>
2014-12-25 10:10:00 +0800

    自动同步 mygit

--- save_local.py
+++ save_local.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import urllib2
+import time
+#公司有多条宽带, ip 会变化,因此用一个 list 来存放,避免反复写 hosts
+ip_list = []
 
 
 def saveHosts(ip):
@@ -18,6 +21,13 @@ def saveHosts(ip):
     f.close()
 
 if __name__ == '__main__':
-    ip = urllib2.urlopen('http://42.96.204.146:9001/getIP').read()
-    if ip != '':
-        saveHosts(ip)
+    while True:
+        new_ip = urllib2.urlopen('http://42.96.204.146:9001/getIP').read()
+        if new_ip != '' and new_ip not in ip_list:
+            saveHosts(new_ip)
+            ip_list.append(new_ip)
+            if len(ip_list) > 4:
+                ip_list = []
+        else:
+            print 'same ip'
+        time.sleep(5)
